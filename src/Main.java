c0b44e6760a7df2f03995ba55d20d3b9dd38f2e5
Serge Dri <drigaserge@gmail.com>
2022-12-25 14:38:49 +0300

    Extra_Variables Done

--- src/Main.java
+++ src/Main.java
@@ -43,10 +43,37 @@ public class Main {
         var discount = 0.1;
         var total = bouquet + card - (bouquet + card)*discount;
         var overallDiscount = (bouquet + card)*discount;
-        System.out.println("Стоимость всех покупок " + total + " рублей, размер скидки " + overallDiscount);
+        System.out.println("Стоимость всех покупок " + total + " рублей, размер скидки " + overallDiscount + "\n");
     }
     public static void task5 () {
-        System.out.println("Дополнительное Задание №1");
+        System.out.println("Дополнительное Задание №5");
+        var oranges = 73;
+        var discOranges = 0.15;
+        var kgOranges = 10;
+        var bananas = 95;
+        var kgBananas = 2;
+        var total = oranges*kgOranges*discOranges + bananas*kgBananas;
+        System.out.println("Куплено " + kgOranges+ " кг апельсинов и " + kgBananas + " кг бананов");
+        System.out.println("Сумма покупки " + total + "\n");
+
+        var champagne = 2870;
+        var bread = 27;
+        var discChampagne = 0.5;
+        var bottles = 7;
+        var loafs = 1;
+        var total1 = champagne*bottles*discChampagne + bread*loafs;
+        System.out.println("Куплено " + bottles+ " бутылок шампансокго и " + loafs + " буханка хлеба");
+        System.out.println("Сумма покупки " + total1 + "\n");
+
+        var champsPrice = 436;
+        var whiteMushroomsPrice = 578;
+        var discWhiteMushrooms = 0.05;
+        var kgchamps = 2.5;
+        var kgWhiteMushrooms = 3.75;
+        var total2 = champsPrice*kgchamps + whiteMushroomsPrice*kgWhiteMushrooms*discWhiteMushrooms;
+        System.out.println("Куплено " + kgchamps+ " кг шампиньонов и " + kgWhiteMushrooms + " кг белых грибов");
+        System.out.println("Сумма покупки " + total2 + "\n");
+
 
     }
     public static void task6 () {
@@ -54,8 +81,8 @@ public class Main {
         double x = 0.40;
         double y = 0.87;
         double a = 11*x + y;
-        double b = (x+10*y)- 15*(x/y);
-        System.out.println("11*x + y = " + a +"(x+10*y)- 15*(x/y) = " + b);
+        double b = (x+10*y) - 15*(x/y);
+        System.out.printf("11*x + y = %.3f" + a + "  (x+10*y)- 15*(x/y) = " + b);
 // как ограничить количество знаков после запятой?
     }
 }
